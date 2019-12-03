555170d930a506c0b839535ff8fa8072d37bffe1
Mehrshad Darzi <mehrshad198@gmail.com>
2019-12-03 17:46:45 +0330

    Set Order By

--- templates/chart.php
+++ templates/chart.php
@@ -4,7 +4,7 @@
 
 	$label = array();
 	$data = array();
-	$query = $wpdb->get_results( "SELECT `LASTSALEPRICE`, `LASTSALETIME` FROM `{$wpdb->prefix}prices` WHERE `LASTSALETIME` BETWEEN {$current_time_x_ago}000 AND {$current_time}000", ARRAY_A );
+	$query = $wpdb->get_results( "SELECT `LASTSALEPRICE`, `LASTSALETIME` FROM `{$wpdb->prefix}prices` WHERE `LASTSALETIME` BETWEEN {$current_time_x_ago}000 AND {$current_time}000 ORDER BY `LASTSALETIME` ASC", ARRAY_A );
 	foreach ( $query as $row ) {
 		$label[] = date( "d M H:i",  $row['LASTSALETIME'] / 1000 );
 		$data[]  = $row['LASTSALEPRICE'];
